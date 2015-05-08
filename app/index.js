69e7f390f21cfe47c943f9e3e55822b09a11ec29
Phoenix Tsui <xsy@ingbaobei.com>
2015-05-08 22:35:40 +0800

    Initialize yeoman generator

---
+++ app/index.js
@@ -0,0 +1,85 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+var generators = yeoman.generators;
+
+module.exports = generators.Base.extend({
+  constructor: function() {
+    generators.Base.apply(this, arguments);
+
+    //this.argument('appname', { type: String, required: true });
+  },
+
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the stunning ' + chalk.red('Remarkjs') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'someOption',
+      message: 'Would you like to enable this option?',
+      default: true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      // To access props later use this.props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      this.fs.copy(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json')
+      );
+      this.fs.copy(
+        this.templatePath('_bower.json'),
+        this.destinationPath('bower.json')
+      );
+      this.fs.copy(
+        this.templatePath('gulpfile.js'),
+        this.destinationPath('gulpfile.js')
+      );
+    },
+
+    folder: function() {
+      this.log(this.destinationPath('html/'));
+      this.fs.copy(
+        this.templatePath('html/**'),
+        this.destinationPath('html/')
+      );
+      this.fs.copy(
+        this.templatePath('md/**'),
+        this.destinationPath('md/')
+      );
+    },
+
+    projectfiles: function () {
+      this.fs.copy(
+        this.templatePath('editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+      this.fs.copy(
+        this.templatePath('jshintrc'),
+        this.destinationPath('.jshintrc')
+      );
+    }
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
